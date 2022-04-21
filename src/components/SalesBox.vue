12e2e7fab2d60f9c3d4812bdf635c73374e82935
iakev <kirimikmwongera@gmail.com>
2022-04-21 10:02:11 +0300

    Added a new component that represents sales made in a certain period of time

---
+++ src/components/SalesBox.vue
@@ -0,0 +1,19 @@
+<template>
+     <tr>
+        <td>{{ item.sales.id }}</td>
+        <td>{{ item.products.name }}</td>
+        <td>{{ item.quantity_sold }}</td>
+        <td>{{ item.price_per_unit_retail }}</td>
+        <td>{{ item.sales.sale_amount }}</td>
+        <td>{{ item.sales.time_paid }}</td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: 'SalesBox',
+    props: {
+        sale : Object
+    },
+}
+</script>
