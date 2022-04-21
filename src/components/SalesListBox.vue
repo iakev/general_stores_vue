f57adcc20fc7e8b75bd232817ddbf6a48965cd2b
iakev <kirimikmwongera@gmail.com>
2022-04-21 10:02:59 +0300

    Added component to simplify the sales lists page

---
+++ src/components/SalesListBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <tr>
+        <td><router-link >{{ sale.id }}</router-link></td>
+        <td>{{  }}</td> <!-- to be implemented when adding users-->
+        <td>{{ sale.sale_amount }}</td>
+        <td>{{ sale.sale_amount_paid }}</td>
+    </tr>
+
+</template>
+
+<script>
+export default {
+    name: 'SalesListBox',
+    props: {
+        sale : Object
+    },
+}
+</script>
