a53e1335b59f1ee005a9d4545a9fa6aac574b32d
iakev <kirimikmwongera@gmail.com>
2022-04-21 10:03:29 +0300

    Created a SaleDetail view page

---
+++ src/views/SaleDetail.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="columns is-multiline">
+            <div class="column is-12 title">
+                <p class="has-text-centered">Sale Detail</p>
+            </div>
+            <hr>
+            <div class="field column is-one-third">
+                        <label class="label">Sales Time</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="time">
+                        </div>
+            </div>
+            <div class="field column is-one-third">
+                        <label class="label">Sales Person</label>
+                        <div class="control">
+                            <input type="text" class="input" v-model="user">
+                        </div>
+            </div>
+            <div class="field column is-one-third">
+                        <label class="label">Sales Type</label>
+                                <input type="checkbox">
+                                Is retail
+            </div>
+            <div class="column is-12 box">
+                <table class="table is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Code</th>
+                            <th>Product Details</th>
+                            <th>Available</th>
+                            <th>Packaging</th>
+                            <th>QTY</th>
+                            <th>@Price</th>
+                            <th>Disc</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                        <tbody>
+                            <SalesBox 
+                                v-for="item in itemsales"
+                                v-bind:key="item.id"
+                                v-bind:sale ="sale">
+                    </tbody>
+                </table>
+            </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import SalesBox from '@/components/SalesBox.vue'
+export default {
+    name : 'SalesDetail',
+    components : {
+        SalesBox,
+    },
+    data (){
+        return {
+            sales : [],
+            sales_price : [],
+        }
+    },
+    methods: {
+        async getSales(){
+            this.$store.commit('setIsLoading',true)
+            await axios
+                .get(`api/v1/sales/`)
+                .then(response =>{
+                    this.sales = response.data
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+          
+            this.$store.commit('setIsLoading',false)
+        },
+
+        async getSalesPrice(){
+            this.$store.commit('setIsLoading',true)
+
+        },
+
+        async setPrice(){
+            await axios
+                    .get('api/v1/')
+            this.price_per_unit_retail = this.products.rate_out_retail
+            this.price_per_unit_wholesale =this.products.rate_out_wholesale
+        },
+
+        async chooseSale(){
+            this.$store.commit('setIsLoading',true)
+            await axios
+                     .get('api/v1/sales/')
+                     .then( response => {
+                         this.sales = response.data
+                     })
+                     .catch( error =>{
+                         console.log(error)
+                     })
+            this.$store.commit('setIsLoading',false)
+        },       
+
+        async submitForm(){
+            const data = {
+                // 'products': this.products.id,
+                'products': this.products,
+                'quantity_sold': this.quantity_sold,
+                'price_per_unit_retail' : this.price_per_unit_retail,
+                'price_per_unit_wholesale' : this.price_per_unit_wholesale,
+                'is_retail' : this.is_retail,
+                'sales' : this.sales,
+            }
+            this.$store.commit('setIsLoading',true)
+            await axios
+                    .post('api/v1/sales/receipts/',data)
+                    .then( response => {
+                    })
+                    .catch( error => {
+                        console.log(error)
+                    })
+            this.$store.commit('setIsLoading',false)
+        }
+    }
+}
+</script>
+
+
