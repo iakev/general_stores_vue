d43dcef86e18520a91b3b289984e099ca5e4b9e9
iakev <kirimikmwongera@gmail.com>
2022-05-12 17:04:35 +0300

    Added new view to create new sale with

---
+++ src/views/NewSale.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'NewSale',
+  data () {
+      return {
+
+      }
+  },
+  methods: {
+    
+    async submitForm () {
+      const data = {
+        // 'products': this.products.id,
+        products: this.products,
+        quantity_sold: this.quantity_sold,
+        price_per_unit_retail: this.price_per_unit_retail,
+        price_per_unit_wholesale: this.price_per_unit_wholesale,
+        is_retail: this.is_retail,
+        sales: this.sales
+      }
+      this.$store.commit('setIsLoading', true)
+      await axios
+        .post('api/v1/sales/receipts/', data)
+        .then(response => {
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      this.$store.commit('setIsLoading', false)
+    }
+
+  }
+
+}
+</script>
